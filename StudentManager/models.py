934e6fe498fae946177f98aa9cd22aa49cf30156
Daniel Chan <iam@danielchan.me>
2015-09-19 04:07:12 -0400

    Tutorial Fields
    Added fields for tutorials.
    
    - Name
    - Code
    - Instructor
    - Capacity
    - Starting Time
    - Day of the week when theres a tutorial

--- StudentManager/models.py
+++ StudentManager/models.py
@@ -6,7 +6,7 @@ class Student(models.Model):
     lectures = models.ManyToManyField('Lecture')
 
     def __unicode__(self):
-        return self.name + ' from class of ' + class_of
+        return self.name + ' from class of ' + self.class_of
 
 
 class Lecture(models.Model):
@@ -15,5 +15,13 @@ class Lecture(models.Model):
         pass
 
 class Tutorial(models.Model):
+
+    t_name = models.CharField(max_length=100)
+    t_code = models.CharField(max_length=10)
+    t_instructor = models.CharField(max_length=100)
+    t_capacity = models.IntegerField()
+    t_sTime = models.CharField(max_length=10)
+    t_Day = models.CharField(max_length=15)
+
     def __unicode__(self):
-        pass
+        return self.t_name + ' has a tutorial code of ' + self.t_code + ' with instructor ' + self.t_instructor + ' with a start time ' + self.t_sTime + ' on ' + self.t_Day
