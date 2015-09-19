b51d861c7ca595a12b744c3e142a262c2e431d21
ShreyKumar <shreyansh.kumar@mail.utoronto.ca>
2015-09-19 10:24:00 -0300

    templates file

--- StudentManager/models.py
+++ StudentManager/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
 class Student(models.Model):
+    id = models.IntegerField()
     name = models.CharField(max_length=100)
     class_of = models.IntegerField()
     lectures = models.ManyToManyField('Lecture')
+    friends = models.ManyToManyField('Friend')
 
     def __unicode__(self):
         return self.name + ' from class of ' + self.class_of
     def enroll(self, lecture):
         lecture.enrolled.push(self)
+    def unenroll(self, lecture):
+        for i in range(len(lecture.enrolled)):
+            student = lecture.enrolled[i].id;
+            if student == self.id:
+                lecture.enrolled.pop(i)
+    
 
 class Lecture(models.Model):
 
+    id = models.IntegerField() #id of the lecture
     name = models.CharField(max_length = 100)
     code = models.CharField(max_length = 10)
     instructor = models.CharField(max_length = 100)
